1966b9d7c8b4dd6c6a3e7c2d25a652886229554d
ruritoBlogger <ruriro0125@gmail.com>
2023-03-21 13:26:37 +0900

    :art: 見た目を軽く整えた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,38 @@
 import { css } from "@emotion/css";
 import { NextPage } from "next";
-import { Typography } from "@mui/material";
+import {
+  Button,
+  Container,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 const App: NextPage = () => {
   return (
-    <div className={rootStyle}>
-      <Typography>voicevox + chatGPT</Typography>
-    </div>
+    <Container maxWidth={"xl"}>
+      <Stack spacing={2}>
+        <Typography variant={"h4"}>
+          VoiceVox + ChatGPT でお手軽会話App
+        </Typography>
+        <Grid container spacing={1} alignItems={"center"}>
+          <Grid item xs={8}>
+            <TextField
+              fullWidth
+              label={"VoiceVoxにしゃべらせたい内容"}
+              variant={"outlined"}
+            />
+          </Grid>
+          <Grid item xs>
+            <Button variant={"contained"} size={"large"}>
+              再生
+            </Button>
+          </Grid>
+        </Grid>
+      </Stack>
+    </Container>
   );
 };
-
-const rootStyle = css`
-  && {
-    height: 100vh;
-    width: 100vw;
-  }
-`;
 
 export default App;
