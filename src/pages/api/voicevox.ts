e0b674409120d1aff4fa46093ccaa73e828b1a3c
ruritoBlogger <ruriro0125@gmail.com>
2023-03-25 01:39:23 +0900

    :recycle: API Routes 側で VoiceVox から音声情報を取得する部分をリファクタリングした

--- src/pages/api/voicevox.ts
+++ src/pages/api/voicevox.ts
@@ -27,35 +27,12 @@ const handler: NextApiHandler = async (
     body: JSON.stringify(parsedData),
   });
 
-  const filePath = "tmp_voice.wav";
-  const talkArrayBuffer = await talkResult.arrayBuffer();
-  const talkDataView = await new DataView(talkArrayBuffer);
-  fs.writeFileSync(filePath, talkDataView);
-  const stat = fs.statSync(filePath);
-  const fileSize = stat.size;
-  const range = req.headers.range;
-  if (range) {
-    const parts = range.replace(/bytes=/, "").split("-");
-    const start = parseInt(parts[0], 10);
-    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-    const chunksize = end - start + 1;
-    const file = fs.createReadStream(filePath, { start, end });
-    const head = {
-      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
-      "Accept-Ranges": "bytes",
-      "Content-Length": chunksize,
-      "Content-Type": "audio/wav",
-    };
-    res.writeHead(206, head);
-    file.pipe(res);
-  } else {
-    const head = {
-      "Content-Length": fileSize,
-      "Content-Type": "audio/wav",
-    };
-    res.writeHead(200, head);
-    fs.createReadStream(filePath).pipe(res);
-  }
+  const arrayBuffer = await talkResult.arrayBuffer();
+  const buffer = await Buffer.from(arrayBuffer);
+  res.setHeader("Content-Type", "audio/wav");
+  res.setHeader("Content-Length", buffer.length);
+  res.setHeader("Content-Disposition", "attachment; filename=audio.wav");
+  res.send(buffer);
 };
 
 export default handler;
