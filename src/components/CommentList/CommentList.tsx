0700629dd48d2ba7370559294ab8edb3281ea836
ruritoBlogger <ruriro0125@gmail.com>
2023-04-01 20:56:03 +0900

    :sparkles: 過去の発言にボタンと送信日付を表示するように実装

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,15 +1,31 @@
-import { List, ListItem, ListItemText } from "@mui/material";
+import {
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@mui/material";
 import { useSuspenseQuery_experimental as useSuspenseQuery } from "@apollo/client";
 import { CommentsDocument } from "../../../graphql/dist/client/graphql";
+import { PlayArrow } from "@mui/icons-material";
 
 export const CommentList = (): JSX.Element => {
   const { data } = useSuspenseQuery(CommentsDocument);
+
   return (
     <>
       <List>
         {data.comments.map((comment) => (
           <ListItem key={comment.id}>
-            <ListItemText primary={comment.content} />
+            <ListItemAvatar>
+              <IconButton>
+                <PlayArrow />
+              </IconButton>
+            </ListItemAvatar>
+            <ListItemText
+              primary={comment.content}
+              secondary={comment.createdAt.toString()}
+            />
           </ListItem>
         ))}
       </List>
