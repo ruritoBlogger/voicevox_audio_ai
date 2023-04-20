fe9aec7933bd21a39ad2d7c5a451a281cd4b1cf3
ruritoBlogger <ruriro0125@gmail.com>
2023-04-20 13:16:18 +0900

    :art: トーク画面のUIを軽く整えた

--- src/pages/talk.tsx
+++ src/pages/talk.tsx
@@ -1,9 +1,27 @@
 import { NextPage } from "next";
+import { Container, Grid, IconButton, Stack, TextField } from "@mui/material";
+import { Mic, Send, StopCircle } from "@mui/icons-material";
 
 const Talk: NextPage = () => {
   return (
     <>
-      <p>voicevox とおしゃべりするよ</p>
+      <Container maxWidth={"xl"}>
+        <Stack spacing={2}>
+          <Grid container spacing={1} alignItems={"center"}>
+            <Grid item xs={11}>
+              <TextField fullWidth label={"メッセージを入力または音声入力"} />
+            </Grid>
+            <Grid item xs>
+              <IconButton>
+                <Mic />
+              </IconButton>
+              <IconButton>
+                <Send />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Stack>
+      </Container>
     </>
   );
 };
