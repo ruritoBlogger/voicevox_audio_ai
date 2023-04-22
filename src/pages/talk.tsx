b378be72b31e618f67ebf4a6efed4a37cc1e86df
ruritoBlogger <ruriro0125@gmail.com>
2023-04-23 00:35:02 +0900

    :art: Talk 画面のテキストフィールドを画面下に表示するように変更

--- src/pages/talk.tsx
+++ src/pages/talk.tsx
@@ -1,37 +1,67 @@
 import { NextPage } from "next";
-import { Container, Grid, IconButton, Stack, TextField } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Container,
+  Grid,
+  IconButton,
+  Stack,
+  TextField,
+} from "@mui/material";
 import { Mic, Send, StopCircle } from "@mui/icons-material";
 import { ChatList } from "@components/ChatList";
 
 const Talk: NextPage = () => {
   return (
-    <>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        minHeight: "100vh",
+      }}
+    >
       <Container
         maxWidth={"lg"}
-        style={{ width: "96%", height: "96%", margin: "auto" }}
+        style={{
+          height: "100%",
+          padding: "32px",
+          display: "flex",
+          flexDirection: "column",
+        }}
       >
-        <Stack spacing={2}>
+        <Grid
+          container
+          spacing={2}
+          direction={"column"}
+          justifyContent={"flex-end"}
+          style={{ flex: "1" }}
+        >
           <ChatList chatList={["こんにちは", "テスト"]} />
-          <Grid container spacing={1} alignItems={"center"}>
-            <Grid item xs={11}>
-              <TextField
-                fullWidth
-                multiline
-                label={"メッセージを入力または音声入力"}
-              />
-            </Grid>
-            <Grid item xs>
-              <IconButton>
-                <Mic />
-              </IconButton>
-              <IconButton>
-                <Send />
-              </IconButton>
-            </Grid>
+        </Grid>
+        <Grid
+          container
+          spacing={1}
+          alignItems={"center"}
+          style={{ padding: "0 10%" }}
+        >
+          <Grid item xs={10}>
+            <TextField
+              fullWidth
+              multiline
+              label={"メッセージを入力または音声入力"}
+            />
           </Grid>
-        </Stack>
+          <Grid item xs>
+            <IconButton>
+              <Mic />
+            </IconButton>
+            <IconButton>
+              <Send />
+            </IconButton>
+          </Grid>
+        </Grid>
       </Container>
-    </>
+    </div>
   );
 };
 
