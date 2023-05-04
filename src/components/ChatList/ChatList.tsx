53f45353fc9d38136cbdf68096769d8e780e02b8
ruritoBlogger <ruriro0125@gmail.com>
2023-05-04 23:38:15 +0900

    :art: 発言者に応じてチャットの表示方法を変更

--- src/components/ChatList/ChatList.tsx
+++ src/components/ChatList/ChatList.tsx
@@ -1,6 +1,7 @@
 import { Grid, Typography } from "@mui/material";
 import { useSuspenseQuery_experimental as useSuspenseQuery } from "@apollo/client";
 import { CommentsDocument } from "../../../graphql/dist/client/graphql";
+import { css } from "@emotion/css";
 
 export const ChatList = (): JSX.Element => {
   const { data } = useSuspenseQuery(CommentsDocument);
@@ -9,10 +10,11 @@ export const ChatList = (): JSX.Element => {
     <>
       <Grid container spacing={1} direction={"column"} alignItems={"center"}>
         {data.comments.map((comment) => {
-          // TODO: author の情報を用いて表示内容を変更する
           return (
             <Grid item key={comment.id}>
-              <Typography variant={"body2"}>{comment.content}</Typography>
+              <div className={paperStyle} data-author={comment.author}>
+                <Typography variant={"body2"}>{comment.content}</Typography>
+              </div>
             </Grid>
           );
         })}
@@ -20,3 +22,53 @@ export const ChatList = (): JSX.Element => {
     </>
   );
 };
+
+const paperStyle = css`
+  && {
+    position: relative;
+    display: inline-block;
+    margin: 1.5em 0 1.5em 15px;
+    padding: 7px 10px;
+    border: solid 3px #555;
+    box-sizing: border-box;
+  }
+
+  &&:before,
+  &&:after {
+    content: "";
+    position: absolute;
+    top: 50%;
+  }
+
+  &&[data-author="AI"]:before {
+    left: -24px;
+    margin-top: -12px;
+    border: 12px solid transparent;
+    border-right: 12px solid #fff;
+    z-index: 2;
+  }
+
+  &&[data-author="AI"]:after {
+    left: -30px;
+    margin-top: -14px;
+    border: 14px solid transparent;
+    border-right: 14px solid #555;
+    z-index: 1;
+  }
+
+  &&[data-author="USER"]:before {
+    right: -24px;
+    margin-top: -12px;
+    border: 12px solid transparent;
+    border-left: 12px solid #fff;
+    z-index: 2;
+  }
+
+  &&[data-author="USER"]:after {
+    right: -30px;
+    margin-top: -14px;
+    border: 14px solid transparent;
+    border-left: 14px solid #555;
+    z-index: 1;
+  }
+`;
