58083ce476dc1a0bd8c7ca44de49fe3c7a4c5ffd
ruritoBlogger <ruriro0125@gmail.com>
2023-05-04 23:43:30 +0900

    :wrench: 初期データを修正

--- src/seed.ts
+++ src/seed.ts
@@ -20,6 +20,26 @@ async function main() {
       author: "AI",
     },
   });
+
+  await prisma.comment.upsert({
+    where: { id: 3 },
+    update: {},
+    create: {
+      id: 3,
+      content: "今日の天気を教えて",
+      author: "USER",
+    },
+  });
+
+  await prisma.comment.upsert({
+    where: { id: 4 },
+    update: {},
+    create: {
+      id: 4,
+      content: "今日は晴れなのだ",
+      author: "AI",
+    },
+  });
 }
 main()
   .then(async () => {
